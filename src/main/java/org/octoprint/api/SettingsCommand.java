227b915ee42a00dda10152dbd6b15497ef7838fa
Rob Weber <rweber@ecec.com>
2017-07-26 14:48:05 -0500

    dont need these

--- src/main/java/org/octoprint/api/SettingsCommand.java
+++ src/main/java/org/octoprint/api/SettingsCommand.java
@@ -1,8 +1,6 @@
 package org.octoprint.api;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.json.simple.JSONArray;
--- src/test/java/org/octoprint/api/test/SettingsCommandTest.java
+++ src/test/java/org/octoprint/api/test/SettingsCommandTest.java
@@ -1,8 +1,6 @@
 package org.octoprint.api.test;
 
 import static org.junit.Assert.*;
-
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Before;
