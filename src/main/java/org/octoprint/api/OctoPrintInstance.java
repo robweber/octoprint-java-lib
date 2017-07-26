4abe6e7756ceee5b40fee7b57c79b362e78aa6c9
Rob Weber <rweber@ecec.com>
2017-07-26 13:10:57 -0500

    wrong import

--- src/main/java/org/octoprint/api/OctoPrintInstance.java
+++ src/main/java/org/octoprint/api/OctoPrintInstance.java
@@ -3,10 +3,10 @@ package org.octoprint.api;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
-import sun.net.www.protocol.http.HttpURLConnection;
 
 /**
  * Stores information on the OctoPrint instance for communication. Used by all OctoPrintCommand classes to send/receive data. 
