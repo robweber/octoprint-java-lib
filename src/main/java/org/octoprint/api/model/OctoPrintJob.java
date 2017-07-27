75a25d24df61a366783ebcdb5a7a5b35b376c3d9
Hendrik Motza <admin@hendrik-motza.de>
2017-07-27 04:44:57 +0200

    providing details about estimated filament consumption of the current job

--- src/main/java/org/octoprint/api/model/OctoPrintJob.java
+++ src/main/java/org/octoprint/api/model/OctoPrintJob.java
@@ -3,18 +3,17 @@ package org.octoprint.api.model;
 import org.json.simple.JSONAware;
 import org.json.simple.JSONObject;
 import org.octoprint.api.util.JSONLoader;
-import org.octoprint.api.util.JSONUtils;
 
 /**
  * Representation of an OctoPrint Job object. http://docs.octoprint.org/en/master/api/datamodel.html#sec-api-datamodel-jobs-job
- * 
+ *
  * @author rweber
- * 
+ *
  */
 public class OctoPrintJob implements JSONAware, JSONLoader {
 	private JSONObject m_job = null;
 	private JobProgress m_progress = null;
-	
+
 	public OctoPrintJob() {
 
 	}
@@ -24,39 +23,52 @@ public class OctoPrintJob implements JSONAware, JSONLoader {
 	 */
 	public String getName(){
 		String result = null;
-		
+
 		if(m_job.containsKey("file"))
 		{
 			JSONObject file = (JSONObject)m_job.get("file");
-			
+
 			if(file.get("name") != null)
 			{
 				result = file.get("name").toString();
 			}
-			
+
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * @return the estimated print time for the file, in seconds
 	 */
 	public Long getEstimatedPrintTime(){
 		return new Long(m_job.get("estimatedPrintTime").toString());
 	}
-	
+
 	/**
 	 * @return current job progress
 	 */
 	public JobProgress getJobProgress(){
 		return m_progress;
 	}
-	
+
+	public FilamentDetails getFilamentDetails(final int toolIndex) {
+		JSONObject m_filament = (JSONObject) m_job.get("filament");
+		if(m_filament == null) {
+			return null;
+		}
+		if(toolIndex != 0 || !(m_filament.containsKey("length") && m_filament.containsKey("volume"))) {
+			m_filament = (JSONObject) m_filament.get("tool"+toolIndex);
+		}
+		final FilamentDetails details = new FilamentDetails();
+		details.loadJSON(m_filament);
+		return details;
+	}
+
 	@Override
 	public void loadJSON(JSONObject json) {
 		m_job = (JSONObject)json.get("job");
-		
+
 		if(json.containsKey("progress"))
 		{
 			m_progress = new JobProgress();
@@ -68,35 +80,62 @@ public class OctoPrintJob implements JSONAware, JSONLoader {
 	public String toJSONString() {
 		return m_job.toJSONString();
 	}
-	
+
 	public class JobProgress implements JSONAware, JSONLoader {
 		private JSONObject m_json = null;
-		
+
 		public JobProgress(){
-			
+
 		}
-		
+
 		public Double percentComplete(){
 			return new Double(m_json.get("completion").toString());
 		}
-		
+
 		public Long elapsedTime(){
 			return new Long(m_json.get("printTime").toString());
 		}
-		
+
 		public Long timeRemaining(){
 			return new Long(m_json.get("printTimeLeft").toString());
 		}
-		
+
 		@Override
 		public void loadJSON(JSONObject json) {
-			m_json = json;	
+			m_json = json;
 		}
 
 		@Override
 		public String toJSONString() {
 			return m_json.toJSONString();
 		}
-		
+
+	}
+
+	public class FilamentDetails implements JSONAware, JSONLoader {
+		private JSONObject m_json = null;
+
+		public FilamentDetails(){
+
+		}
+
+		public Long length(){
+			return new Long(m_json.get("length").toString());
+		}
+
+		public Double volume(){
+			return new Double(m_json.get("volume").toString());
+		}
+
+		@Override
+		public void loadJSON(JSONObject json) {
+			m_json = json;
+		}
+
+		@Override
+		public String toJSONString() {
+			return m_json.toJSONString();
+		}
+
 	}
 }
