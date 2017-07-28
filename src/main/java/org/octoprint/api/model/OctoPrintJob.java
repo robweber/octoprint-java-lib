1e39ecf77c4d06cd3cd396bcea58f40a7a9442c7
DataWorm <your.dataworm@googlemail.com>
2017-07-28 19:54:22 +0200

    Filament details (#5)
    Looks good. There are a few minor code formatting changes in the main getFilamentDetails section but rather than back and forth I'll tweak them after the merge. Nothing affecting functionality, more to just make things look consistent. Thanks for adding! I'll update the javadoc and such in the master.

--- src/main/java/org/octoprint/api/model/OctoPrintJob.java
+++ src/main/java/org/octoprint/api/model/OctoPrintJob.java
@@ -3,18 +3,17 @@ package org.octoprint.api.model;
 import org.json.simple.JSONAware;
 import org.json.simple.JSONObject;
 import org.octoprint.api.util.JSONLoader;
-import org.octoprint.api.util.JSONUtils;
 
 /**
  * Representation of an OctoPrint Job object. http://docs.octoprint.org/en/master/api/datamodel.html#sec-api-datamodel-jobs-job
- * 
+ *
  * @author rweber
- * 
+ *
  */
 public class OctoPrintJob implements JSONAware, JSONLoader {
 	private JSONObject m_job = null;
 	private JobProgress m_progress = null;
-	
+
 	public OctoPrintJob() {
 
 	}
@@ -24,39 +23,61 @@ public class OctoPrintJob implements JSONAware, JSONLoader {
 	 */
 	public String getName(){
 		String result = null;
-		
+
 		if(m_job.containsKey("file"))
 		{
 			JSONObject file = (JSONObject)m_job.get("file");
-			
+
 			if(file.get("name") != null)
 			{
 				result = file.get("name").toString();
 			}
-			
+
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * @return the estimated print time for the file, in seconds
 	 */
 	public Long getEstimatedPrintTime(){
 		return new Long(m_job.get("estimatedPrintTime").toString());
 	}
-	
+
 	/**
 	 * @return current job progress
 	 */
 	public JobProgress getJobProgress(){
 		return m_progress;
 	}
-	
+
+	/**
+	 * Returns information about the expected filament consumption regarding the current print job. Can be {@code null} if {@code toolIndex} is out of range or if details couldn't be retrieved.
+	 *
+	 * @param toolIndex Number of the tool to get details about
+	 * @return filament consumption details
+	 */
+	public FilamentDetails getFilamentDetails(final int toolIndex) {
+		if(this.m_job == null) {
+			return null;
+		}
+		JSONObject m_filament = (JSONObject) this.m_job.get("filament");
+		if(m_filament == null) {
+			return null;
+		}
+		if(toolIndex != 0 || !(m_filament.containsKey("length") && m_filament.containsKey("volume"))) {
+			m_filament = (JSONObject) m_filament.get("tool"+toolIndex);
+		}
+		final FilamentDetails details = new FilamentDetails();
+		details.loadJSON(m_filament);
+		return details;
+	}
+
 	@Override
 	public void loadJSON(JSONObject json) {
 		m_job = (JSONObject)json.get("job");
-		
+
 		if(json.containsKey("progress"))
 		{
 			m_progress = new JobProgress();
@@ -68,35 +89,87 @@ public class OctoPrintJob implements JSONAware, JSONLoader {
 	public String toJSONString() {
 		return m_job.toJSONString();
 	}
-	
+
 	public class JobProgress implements JSONAware, JSONLoader {
 		private JSONObject m_json = null;
-		
+
 		public JobProgress(){
-			
+
 		}
-		
+
 		public Double percentComplete(){
 			return new Double(m_json.get("completion").toString());
 		}
-		
+
 		public Long elapsedTime(){
 			return new Long(m_json.get("printTime").toString());
 		}
-		
+
 		public Long timeRemaining(){
 			return new Long(m_json.get("printTimeLeft").toString());
 		}
-		
+
 		@Override
 		public void loadJSON(JSONObject json) {
-			m_json = json;	
+			m_json = json;
 		}
 
 		@Override
 		public String toJSONString() {
 			return m_json.toJSONString();
 		}
-		
+
+	}
+
+	/**
+	 * Provides information about filament usage.
+	 */
+	public static final class FilamentDetails implements JSONAware, JSONLoader {
+		private JSONObject m_json = null;
+
+		private FilamentDetails(){
+
+		}
+
+		/**
+		 * Returns the length of filament. {@code null} if length is not provided.
+		 *
+		 * @return length in mm
+		 */
+		public Long length(){
+			if(this.m_json == null || !this.m_json.containsKey("length")) {
+				return null;
+			}
+			return new Long(this.m_json.get("length").toString());
+		}
+
+		/**
+		 * Returns the volume of filament. {@code null} if volume is not provided.
+		 *
+		 * @return volume in cm³
+		 */
+		public Double volume(){
+			if(this.m_json == null || !this.m_json.containsKey("volume")) {
+				return null;
+			}
+			return new Double(this.m_json.get("volume").toString());
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public void loadJSON(final JSONObject json) {
+			m_json = json;
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public String toJSONString() {
+			return this.m_json.toJSONString();
+		}
+
 	}
 }
--- src/test/java/org/octoprint/api/test/JobCommandTest.java
+++ src/test/java/org/octoprint/api/test/JobCommandTest.java
@@ -8,19 +8,20 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import org.octoprint.api.JobCommand;
 import org.octoprint.api.OctoPrintInstance;
+import org.octoprint.api.model.OctoPrintJob;
 import org.octoprint.api.model.OctoPrintJob.JobProgress;
 import org.octoprint.api.test.util.JSONAnswer;
 
 /**
- * 
+ *
  * Test aspects of the JobCommand class
- * 
+ *
  * @author rweber
  *
  */
 public class JobCommandTest {
 	private JobCommand command = null;
-	
+
 	public JobCommandTest() {
 		// TODO Auto-generated constructor stub
 	}
@@ -29,43 +30,64 @@ public class JobCommandTest {
 	public void beforeTest(){
 		//create a fake instance for http simulation
 		OctoPrintInstance i = Mockito.mock(OctoPrintInstance.class,new JSONAnswer("job.json"));
-		
+
 		command = new JobCommand(i);
 	}
-	
+
 	@Test
 	public void nameTest(){
-		
+
 		assertEquals("Job Name Correct",command.getJobDetails().getName(),"whistle_v2.gcode");
 	}
-	
+
 	@Test
 	public void estimatedTimeTest(){
-		
+
 		assertEquals("Estimated Time Remaining",(long)8811,command.getJobDetails().getEstimatedPrintTime().longValue());
 	}
-	
+
 	@Test
 	public void percentCompleteTest(){
-		
+
 		//check a job at 23%
 		JobProgress p = command.getJobDetails().getJobProgress();
-		assertEquals("Partial Complete",.23,p.percentComplete(),1);
-		
+		assertEquals("Partial Complete",.23,p.percentComplete(),0.01);
+
 		//check a job at 100%
 		OctoPrintInstance i2 = Mockito.mock(OctoPrintInstance.class,new JSONAnswer("100_job.json"));
 		command = new JobCommand(i2);
-		
+
 		p = command.getJobDetails().getJobProgress();
-		assertEquals("100 Percent Complete",1,p.percentComplete(),1);
+		assertEquals("100 Percent Complete",1,p.percentComplete(),0.01);
 	}
-	
+
+	@Test
+	public void filamentDetailsTest(){
+
+		//check a job with filament length 810 and filament volume 5.36
+		OctoPrintJob.FilamentDetails f = command.getJobDetails().getFilamentDetails(0);
+		assertEquals("Filament length", 810, (long) f.length());
+		assertEquals("Filament volume",5.36, f.volume(),0.01);
+
+		//check a job at 100%
+		OctoPrintInstance i2 = Mockito.mock(OctoPrintInstance.class,new JSONAnswer("2extruders_job.json"));
+		command = new JobCommand(i2);
+
+		//check a job with filament0 length 810 and filament0 volume 5.36 and filament1 length 10 and filament1 volume 1
+		f = command.getJobDetails().getFilamentDetails(0);
+		assertEquals("Filament length", 818, (long) f.length());
+		assertEquals("Filament volume",1.97, f.volume(),0.01);
+		f = command.getJobDetails().getFilamentDetails(1);
+		assertEquals("Filament length", 10, (long) f.length());
+		assertEquals("Filament volume",1, f.volume(),0.01);
+	}
+
 	@Test
 	public void elaspedTimeTest(){
 		JobProgress p = command.getJobDetails().getJobProgress();
 		assertEquals("Elapsed Time",(long)276,p.elapsedTime().longValue());
 	}
-	
+
 	@Test
 	public void timeRemainingTest(){
 		JobProgress p = command.getJobDetails().getJobProgress();
